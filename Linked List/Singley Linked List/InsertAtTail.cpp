033a813a6656bf9fb412044e4b76b49ac3b99d47
Naveen Sahu <naveensahu1402@gmail.com>
2025-02-01 18:05:35 +0530

    WAP to insert new node at the end of the linked list

---
+++ Linked List/Singley Linked List/InsertAtTail.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node {
+	public:
+		int data;
+		Node* next;
+		
+		Node(int data) {
+			this -> data = data;
+			this -> next = NULL;
+		}
+};
+
+void insertAtTail(Node* &tail, int d) {
+	
+	//create new node
+	Node* newNode = new Node(d);
+
+  tail -> next = newNode;
+	tail = newNode;
+}
+
+void print(Node* &head) {
+	
+	Node* temp = head;
+	while(temp != NULL) {
+		cout << temp -> data << " ";
+		temp = temp -> next;
+	}
+	cout << endl;
+}
+
+int main() {
+	
+	Node* node1 = new Node(10);
+	Node* head = node1;
+	Node* tail = node1;
+	print(head);
+
+	insertAtTail(tail, 15);
+	print(head);
+
+  insertAtTail(tail, 20);
+	print(head);
+
+
+}
